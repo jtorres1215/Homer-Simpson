5a45c3c679474140ef47b7bb61d8d2c15365854f
Justin T <43036766+jtorres1215@users.noreply.github.com>
2019-03-26 08:25:35 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,18 @@
+$("#profile-picture").click(function() {
+    $("#interests").css("background-color", "yellow");
+});
+$(".bart").click(function() {
+    $("#header").css("background-color", "blue");
+});
+$("#profile-picture").click(function() {
+    alert("Homer's Profile");
+});
+$(".bart").click(function() {
+    alert("Don't have a cow man");
+});
+$(".ned").click(function() {
+    alert("Hidely-ho neighborino");
+});
+$(".maggie").click(function() {
+    alert("*pacifier noise*");
+});
